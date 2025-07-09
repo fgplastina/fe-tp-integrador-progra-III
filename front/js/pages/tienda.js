ad2cf97b3f277f71dc16cc97ce2b4940d9bb773e
Federico Plastina <40448913+fgplastina@users.noreply.github.com>
2025-07-09 17:34:56 -0300

    almacenamos carrito en local storage

--- front/js/pages/tienda.js
+++ front/js/pages/tienda.js
@@ -2,11 +2,35 @@ import { fetchData, createElement } from '../utils.js';
 
 const mainSection = document.querySelector('#main .tiles');
 
+let cart = loadCart();
+
 async function init() {
   const products = await fetchData('/products');
   const categories = await fetchData('/categories');
   renderFilters(categories, products);
   renderProducts(products);
+}
+
+
+function loadCart() {
+  const data = localStorage.getItem('cart');
+  return data ? JSON.parse(data) : [];
+}
+
+function saveCart() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function addToCart(id, name, price) {
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  cart.push({ id, name, price });
+  localStorage.setItem('cart', JSON.stringify(cart));
+  alert(`${name} agregado al carrito`);
+}
+
+function updateCartCounter() {
+  const counter = document.getElementById('cart-counter');
+  if (counter) counter.textContent = `🛒 ${cart.length}`;
 }
 
 function renderFilters(categories, products) {
@@ -44,20 +68,35 @@ function renderFilters(categories, products) {
 
 function renderProducts(productList) {
   mainSection.innerHTML = '';
+
   productList.forEach(p => {
     const article = document.createElement('article');
     article.className = 'style1';
+
+    article.dataset.id = p.id;
+    article.dataset.name = p.name;
+    article.dataset.price = p.price;
+
     article.innerHTML = `
       <span class="image"><img src="${p.image_url}" alt="${p.name}" /></span>
-      <a href="generic.html">
+      <a href="#!">
         <h2>${p.name}</h2>
         <div class="content">
           <p>$${p.price.toFixed(2)}</p>
         </div>
       </a>
     `;
+
+    article.addEventListener('click', () => {
+      const id = parseInt(article.dataset.id);
+      const name = article.dataset.name;
+      const price = parseFloat(article.dataset.price);
+      addToCart(id, name, price);
+    });
+
     mainSection.appendChild(article);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
