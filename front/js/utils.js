9a76c06754111b6899d2fc726f348b4a1ad5a4a2
Federico Plastina <40448913+fgplastina@users.noreply.github.com>
2025-07-11 15:17:55 -0300

    agregamos postData en utils

--- front/js/utils.js
+++ front/js/utils.js
@@ -13,3 +13,13 @@ export function createElement(tag, className = '', inner = '') {
   el.innerHTML = inner;
   return el;
 }
+
+export async function postData(endpoint, data) {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error('Error al enviar los datos');
+  return res.json();
+}
