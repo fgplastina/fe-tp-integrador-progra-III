6e1427b5cce23b232a5dd484afcb8e99703225fa
Ivan <ivanlaurito@hotmail.com>
2025-07-11 05:12:26 -0300

    saludo y carrito

--- front/js/carrito.js
+++ front/js/carrito.js
@@ -1,94 +1,118 @@
-let arrayCarrito = [];
-const contenedorCarrito = document.getElementById("items-carrito");
+document.addEventListener('DOMContentLoaded', () => {
 
-function crearBotonesAgregarCarrito() {
-    const contenedorProductos = document.querySelector(".contenedor-productos");
-    const botones = contenedorProductos.querySelectorAll('button');
+    console.log('carrito.js cargado');
 
-    botones.forEach(function(boton) {
-    boton.addEventListener("click", function() {
-            arrayCarrito.push(arrayFrutas.find(fruta => fruta.id == boton.id));
-            mostrarCarrito();
+    let carrito = [];
+    const contenedorCarrito = document.getElementById("items-carrito");
+
+    function actualizarCarrito() {
+        contenedorCarrito.innerHTML = '';
+        carrito.forEach(producto => {
+            const item = document.createElement('li');
+            item.className = 'bloque-item';
+            item.innerHTML = `
+            <p class="nombre-item">${producto.nombre} - ${producto.precio}</p>
+            <button id=${producto.id} class="boton-eliminar">Eliminar</button>
+            `;
+            contenedorCarrito.appendChild(item);
+
+            const conteo = document.createElement('div');
+            conteo.className = 'botonesContadorCarrito';
+            conteo.innerHTML = `
+            <div class="botonesContadorCarrito">
+                <button onclick="restarCantidadCarrito(${producto.id})">-</button>
+                <span id="${producto.id}">1</span>
+                <button onclick="sumarCantidadCarrito(${producto.id})">+</button>
+            </div>
+            `;
+
+            item.appendChild(conteo);
         });
-    });
-}
 
-function mostrarCarrito() {
-    contenedorCarrito.innerHTML = '';
-    arrayCarrito.forEach(fruta => {
-        const item = document.createElement('li');
-        item.className = 'bloque-item';
-        item.innerHTML = `
-        <p class="nombre-item">${fruta.nombre} - ${fruta.precio}</p>
-        <button id=${fruta.id} class="boton-eliminar">Eliminar</button>
-        `;
-        contenedorCarrito.appendChild(item);
-
-        const conteo = document.createElement('div');
-        conteo.className = 'botonesContadorCarrito';
-        conteo.innerHTML = `
-        <div class="botonesContadorCarrito">
-            <button onclick="restarCantidadCarrito(${fruta.id})">-</button>
-            <span id="${fruta.id}">1</span>
-            <button onclick="sumarCantidadCarrito(${fruta.id})">+</button>
-        </div>
-        `;
-
-        item.appendChild(conteo);
-    });
-
-    const botones = document.querySelectorAll('.boton-eliminar');
-    botones.forEach(boton => {
-        boton.addEventListener('click', function() {
-            eliminarProducto(boton.id);
-        });
-    });
-    
-    sumarCantidadCarrito();
-    actualizarPrecioCarrito();
-}
-
-function eliminarProducto(idFruta) {
-    arrayCarrito = arrayCarrito.filter((fruta) => fruta.id !== Number(idFruta));
-    mostrarCarrito();
-}
-
-function almacenarCarritoLocalStorage() {
-    const jsonFrutas = JSON.stringify(arrayFrutas);
-    localStorage.setItem("frutas", jsonFrutas);
-
-}
-
-function recuperarCarritoLocalStorage() {
-    const datosFrutasCarrito = localStorage.getItem("frutas");
-
-    if (datosFrutasCarrito) {
-        arrayCarrito = JSON.parse(datosFrutasCarrito);
-        console.log("Se cargaron datos guardados del carrito.");
-    } else {
-    console.log("No se encontraron datos guardados del carrito");
     }
 
-}
+    const productos = document.querySelectorAll('.botones-carrito');
+
+    productos.forEach(producto => {
+    const id = producto.dataset.id;
+    const nombre = producto.dataset.nombre;
+    const precio = parseFloat(producto.dataset.precio);
+    //const cantidadSpan = producto.querySelector('.cantidad');
+
+    producto.querySelector('.boton-sumar').addEventListener('click', (event) => {
+        event.stopPropagation()
+        console.log('click en boton')
+        if (!carrito[id]) {
+        carrito[id] = { nombre, precio, cantidad: 0 };
+        }
+        carrito[id].cantidad++;
+        //cantidadSpan.textContent = carrito[id].cantidad;
+        actualizarCarrito();
+    });
+
+    producto.querySelector('.boton-restar').addEventListener('click', (event) => {
+        event.stopPropagation()
+        if (carrito[id]) {
+        carrito[id].cantidad--;
+        if (carrito[id].cantidad <= 0) {
+            delete carrito[id];
+            cantidadSpan.textContent = 0;
+        } else {
+            cantidadSpan.textContent = carrito[id].cantidad;
+        }
+        actualizarCarrito();
+        }
+    });
+    });
+
+    function eliminarProducto(idFruta) {
+        carrito = carrito.filter((fruta) => fruta.id !== Number(idFruta));
+        mostrarCarrito();
+    }
 
 
-function actualizarContadorCarrito() {
-    const contadorCarrito = document.getElementById("contador-carrito");
-    contenedorCarrito.textContent = arrayCarrito.length;
-}
+    function almacenarCarritoLocalStorage() {
+        const jsonFrutas = JSON.stringify(arrayFrutas);
+        localStorage.setItem("frutas", jsonFrutas);
 
-function sumarCantidadCarrito(id) {
-    const contadorCarrito = document.getElementById(id)
-    parseInt(contadorCarrito.textContent)++;
-}
+    }
 
-function restarCantidadCarrito(id) {
-    const contadorCarrito = document.getElementById(id)
-    parseInt(contadorCarrito.textContent)--;
-}
+    function recuperarCarritoLocalStorage() {
+        const datosFrutasCarrito = localStorage.getItem("frutas");
 
-function actualizarPrecioCarrito() {
-    const precioTotalCarrito = document.getElementById('precio-total');
-    const precioTotal = arrayCarrito.reduce((a, c) => a + c, 0)
-    precioTotalCarrito.textContent = arrayCarrito.length;
-}
+        if (datosFrutasCarrito) {
+            carrito = JSON.parse(datosFrutasCarrito);
+            console.log("Se cargaron datos guardados del carrito.");
+        } else {
+        console.log("No se encontraron datos guardados del carrito");
+        }
+
+    }
+
+
+    function actualizarContadorCarrito() {
+        const contadorCarrito = document.getElementById("contador-carrito");
+        contenedorCarrito.textContent = carrito.length;
+    }
+
+    function sumarAlCarrito() {
+        if (!carrito[id]) {
+        carrito[id] = { nombre, precio, cantidad: 0 };
+        }
+        carrito[id].cantidad++;
+        cantidadSpan.textContent = carrito[id].cantidad;
+        actualizarCarrito();
+    }
+
+
+    function restarCantidadCarrito(id) {
+        const contadorCarrito = document.getElementById(id)
+        parseInt(contadorCarrito.textContent)--;
+    }
+
+    function actualizarPrecioCarrito() {
+        const precioTotalCarrito = document.getElementById('precio-total');
+        const precioTotal = carrito.reduce((a, c) => a + c, 0)
+        precioTotalCarrito.textContent = carrito.length;
+    }
+});
--- front/js/pages/tienda.js
+++ front/js/pages/tienda.js
@@ -7,6 +7,8 @@ async function init() {
   const categories = await fetchData('/categories');
   renderFilters(categories, products);
   renderProducts(products);
+  renderCarrito();
+  asignarEventosCarrito();
 }
 
 function renderFilters(categories, products) {
@@ -36,6 +38,7 @@ function renderFilters(categories, products) {
       const filtered = products.filter(p => p.category_id === parseInt(selected));
       renderProducts(filtered);
     }
+    asignarEventosCarrito();
   };
 
   wrapper.appendChild(select);
@@ -53,10 +56,117 @@ function renderProducts(productList) {
         <h2>${p.name}</h2>
         <div class="content">
           <p>$${p.price.toFixed(2)}</p>
+          <div class="botones-carrito" data-id=${p.id} data-nombre=${p.nombre} data-precio=${p.precio} data-imagen=${p.image_url}>
+            <button class="boton-restar">-</button>
+            <button class="boton-sumar">+</button>
+          </div>
         </div>
       </a>
     `;
     mainSection.appendChild(article);
+  });
+}
+
+function renderCarrito() {
+  let carrito = [];
+  const contenedorCarrito = document.getElementById("items-carrito");
+
+  function actualizarCarrito() {
+      contenedorCarrito.innerHTML = '';
+      carrito.forEach(producto => {
+          const item = document.createElement('li');
+          item.className = 'bloque-item';
+          item.innerHTML = `
+          <p class="nombre-item">${producto.nombre} - ${producto.precio}</p>
+          <button id=${producto.id} class="boton-eliminar">Eliminar</button>
+          `;
+          contenedorCarrito.appendChild(item);
+
+          const conteo = document.createElement('div');
+          conteo.className = 'botonesContadorCarrito';
+          conteo.innerHTML = `
+          <div class="botonesContadorCarrito">
+              <button onclick="restarCantidadCarrito(${producto.id})">-</button>
+              <span id="${producto.id}">1</span>
+              <button onclick="sumarCantidadCarrito(${producto.id})">+</button>
+          </div>
+          `;
+
+          item.appendChild(conteo);
+      });
+  }
+
+  function eliminarProducto(idFruta) {
+      carrito = carrito.filter((fruta) => fruta.id !== Number(idFruta));
+      mostrarCarrito();
+  }
+
+
+  function almacenarCarritoLocalStorage() {
+      const jsonFrutas = JSON.stringify(arrayFrutas);
+      localStorage.setItem("frutas", jsonFrutas);
+
+  }
+
+  function recuperarCarritoLocalStorage() {
+      const datosFrutasCarrito = localStorage.getItem("frutas");
+
+      if (datosFrutasCarrito) {
+          carrito = JSON.parse(datosFrutasCarrito);
+          console.log("Se cargaron datos guardados del carrito.");
+      } else {
+      console.log("No se encontraron datos guardados del carrito");
+      }
+
+  }
+
+
+  function actualizarContadorCarrito() {
+      const contadorCarrito = document.getElementById("contador-carrito");
+      contenedorCarrito.textContent = carrito.length;
+  }
+
+  function sumarAlCarrito() {
+      if (!carrito[id]) {
+      carrito[id] = { nombre, precio, cantidad: 0 };
+      }
+      carrito[id].cantidad++;
+      cantidadSpan.textContent = carrito[id].cantidad;
+      actualizarCarrito();
+  }
+
+
+  function restarCantidadCarrito(id) {
+      const contadorCarrito = document.getElementById(id)
+      parseInt(contadorCarrito.textContent)--;
+  }
+
+  function actualizarPrecioCarrito() {
+      const precioTotalCarrito = document.getElementById('precio-total');
+      const precioTotal = carrito.reduce((a, c) => a + c, 0)
+      precioTotalCarrito.textContent = carrito.length;
+  }
+}
+
+function asignarEventosCarrito() {
+  const productos = document.querySelectorAll('.botones-carrito');
+
+  productos.forEach(producto => {
+    const id = producto.dataset.id;
+    const nombre = producto.dataset.nombre;
+    const precio = parseFloat(producto.dataset.precio);
+
+    producto.querySelector('.boton-sumar').addEventListener('click', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log('click en boton +');
+    });
+
+    producto.querySelector('.boton-restar').addEventListener('click', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log('click en boton -');
+    });
   });
 }
 
